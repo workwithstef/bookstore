3f38255ef89209bf5eabaa1ee41bfe5ed88940e3
Stefan Okolo <stefan.okolo@outlook.com>
2021-02-04 00:25:50 +0000

    touches

--- py_db.py
+++ py_db.py
@@ -3,7 +3,7 @@ from shhh.py import *
 
 class MYSQLConnect:
 
-    def __init__(self, database, host="localhost", user="root", passwd="MY_SQL"):
+    def __init__(self, database, host="localhost", user="root", passwd=MY_SQL):
         self.host = host
         self.user = user
         self.passwd = passwd
@@ -23,20 +23,4 @@ class MYSQLConnect:
         else:
             return self.cur.execute(str(query))
 
-
-books = MYSQLConnect("books")
-
-
-# all_books = books.sql_query("select * from library")
-
-monster_book = books.sql_query("select * from library where book_title IN ('Monster')")
-
-# print(all_books.fetchone())
-
-# mydb = mysql.connector.connect(host="localhost", user="root", passwd="ssss", database="books")
-# cursor = mydb.cursor()
-# book_list = cursor.execute("select * from library")
-
-for i in books.cur:
-    print(i)
 
